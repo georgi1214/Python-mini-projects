eb0c922bc1610973eea5fc549ce71fdf44f5b97d
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2023-03-13 19:24:16 +0200

    Add files via upload

---
+++ MiniProjectsWithRandom/projectMovieLibrrary/class_movie.py
@@ -0,0 +1,6 @@
+class Movie:
+    def __init__(self, title, director, release_year, rating):
+        self.title = title
+        self.director = director
+        self.release_year = release_year
+        self.rating = rating
